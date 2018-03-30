0d88007e359b87d7f1bb3959701620fc0e5d87f9
Ben Schneider <benjamminschneider@gmail.com>
2018-03-29 23:18:25 -0400

    Rewrite the function to reference a JS script file in the package's "inst/js" directory.
    Also add error messages to help the user specify where the resulting JS script file is placed if 'in_html_doc = FALSE'.

--- R/add_javascript.R
+++ R/add_javascript.R
@@ -1,54 +1,25 @@
-add_javascript <- function(in_html_doc = TRUE) {
+add_javascript <- function(in_html_doc = TRUE,
+                           filepath = NULL) {
   
-js_lines <- c("//This JavaScript function changes the src for an image reference", 
-              "function ChangeImageSrc(SelectInput) {", "    // Get the id of the select input, ", 
-              "  var selectId = String(SelectInput.id);", "  // Get the selected value from the select input", 
-              "  var SelectionValue = SelectInput.value;", "  var relevantClass = \"ImageSelection\";", 
-              "  relevantClass = relevantClass + \" \" + selectId;", "", "  // List the inline text elements whose class marks them as being related to the select element", 
-              "  var InlineTextElements = document.getElementsByClassName(relevantClass);", 
-              "", "  // Loop over each inline text element, setting the text inside it to match", 
-              "  // the text content corresponding to the selected option", 
-              "", "  for (i = 0; i < InlineTextElements.length; i++) {", "    AffectedSpan = InlineTextElements[i];", 
-              "    // Retrieve the results list from the span object, in the form of a string", 
-              "    // that can be turned into a JSON object", "    var ResultsListDataString = String(AffectedSpan.getAttribute('resultlistdata', 0));", 
-              "    // Turn the string into a JSON object", "    ResultListData = JSON.parse(ResultsListDataString);", 
-              "    // Extract the selected value from the JSON, make sure that it's a string", 
-              "    mystring = String(ResultListData[SelectionValue]);", "", 
-              "    AffectedSpan.src = ResultListData[SelectionValue];", "  }", 
-              "}", "", "//This JavaScript function changes in-line text", "function ChangeInlineText(SelectInput) {", 
-              "  // Get the id of the select input, ", "  var selectId = String(SelectInput.id);", 
-              "  // Get the selected value from the select input", "  var SelectionValue = SelectInput.value;", 
-              "  var relevantClass = \"InlineTextSelection\";", "  relevantClass = relevantClass + \" \" + selectId;", 
-              "", "  // List the inline text elements whose class marks them as being related to the select element", 
-              "  var InlineTextElements = document.getElementsByClassName(relevantClass);", 
-              "", "  // Loop over each inline text element, setting the text inside it to match", 
-              "  // the text content corresponding to the selected option", 
-              "", "  for (i = 0; i < InlineTextElements.length; i++) {", "    AffectedSpan = InlineTextElements[i];", 
-              "    // Retrieve the results list from the span object, in the form of a string", 
-              "    // that can be turned into a JSON object", "    var ResultsListDataString = String(AffectedSpan.getAttribute('resultlistdata', 0));", 
-              "    // Turn the string into a JSON object", "    ResultListData = JSON.parse(ResultsListDataString);", 
-              "    // Extract the selected value from the JSON, make sure that it's a string", 
-              "    mystring = String(ResultListData[SelectionValue]);", "", 
-              "    AffectedSpan.textContent = ResultListData[SelectionValue];", 
-              "  }", "}", "", "// This adds an event listener to each r_select_html_selector that will,", 
-              "// when the value of the selector changes,", "// run all of the content-swapping functions",
-              "// NOTE: The 'DOMContentLoaded' listener is necessary to make sure that the selector listeners are only added once the document's DOM content has loaded.",
-              "// This allows the script to be placed anywhere in the document, not just at the very end.",
-              "document.addEventListener('DOMContentLoaded', function() {",
-              "var r_select_html_selectors = document.getElementsByClassName('r_select_html_selector');", 
-              "for (var i = 0; i < r_select_html_selectors.length; i++) {", 
-              "    r_select_html_selectors[i].addEventListener('change', function() {ChangeInlineText(this); ChangeImageSrc(this);}, false);", 
-              "}", "});")
-  
-  
+  js_source_file <- system.file("js", "Add_Listener_and_Add_Change_Functions.js",
+                                package = "r_html_select")
 
   if (in_html_doc) {
+    js_lines <- readLines(con = js_source_file)
     writeLines(utf8::utf8_encode(c("<script>",
                                   js_lines,
                                   "</script>")),
                sep = "\n")
   } else {
-    writeLines(utf8::utf8_encode(js_lines))
+    if (is.null(filepath)) {
+      stop("When 'in_html_doc' is FALSE, you must specify a filepath for the 'filepath' argument, such as 'add_listener_and_add_change_functions.js'.")
+    } else {
+      if (!endsWith(filepath, ".js")) {
+        stop("The filepath argument must be a string ending with the '.js' extension.")
+      }
+    }
+    file.copy(from = js_source_file,
+              to = filepath,
+              copy.mode = TRUE)
   }
-
 }
