94525b91eee4a40b62a48828466957f5c6234e55
bschneider <benjamminschneider@gmail.com>
2018-06-27 11:53:04 -0400

    Allow user to specify output HTML as div
    Add a 'type' argument that controls whether the output is a 'div' or a 'span' object.

--- R/sel_inline_text.R
+++ R/sel_inline_text.R
@@ -1,9 +1,14 @@
 sel_inline_text <- function(sel_id = "ts_1",
                             options = list('Option_1' = "The first option's text.",
                                            'Option_2' = "You get it.",
-                                           'Option_3' = "We get it already.")) {
+                                           'Option_3' = "We get it already."),
+                            type = 'span') {
   
-  span_class <- paste0("InlineTextSelection", " ", sel_id)
+  if (!type %in% c("span", "div") {
+    stop("type must be either 'span' or 'div'")
+  }
+  
+  element_class <- paste0("InlineTextSelection", " ", sel_id)
   
   options <- setNames(object = as.list(as.character(options)),
                       nm = names(options))
@@ -11,7 +16,13 @@ sel_inline_text <- function(sel_id = "ts_1",
   result_list_data <- jsonlite::toJSON(options)
   result_list_data <- utf8::utf8_encode(result_list_data)
   
+  if (type == 'span') {
   htmltools::span(options[1],
-                  class = span_class,
+                  class = element_class,
                   resultlistdata = result_list_data)
+  } else if (type == 'div') {
+    htmltools::div(options[1],
+                   class = element_class,
+                   resultlistdata = result_list_data)
+  }
 }
